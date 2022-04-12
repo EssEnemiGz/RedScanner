821266cc3995ece562fcd3157c5117bd3d24b8bc
EssEnemiGz <essenemiggz@gmail.com>
2022-04-12 16:01:51 -0400

    Opciones extras y escaneo de puertos bajo protocolo UDP agregado.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 Escaner de puertos, por EssEnemiGz.
 
 Programado en Python.
+
+Uso básico. 
+
+Argumentos.
+
+-o, indica al programa que debe escanear los puertos abiertos. Luego de esta opción se debe pasar el host y el limite de puertos a escanear.
+
+-tcp, indica que realice el escaneo usando el protocolo TCP.
+
+-udp, indica que realice el escaneoo usando el protocolo UDP.
+
+-all, indica que use el protocolo TCP y UDP por igual.
+
+Ejemplo de uso.
+
+python ports.py -o 8.8.8.8 4000 -udp
--- ports.py
+++ ports.py
@@ -8,17 +8,25 @@ from sys import argv
 
 class main:
     def __init__(self):
-        print("Escaner de red.\nPor: EssEnemiGz")
+        print("\033[1;32m-> Escaner de red.\n Por: EssEnemiGz \033[0m")
     
-    def openPorts(self, host, portRange):
+    def openPorts(self, host, portRange, sock):
         dispo = []
         close = []
-        
+
+        if portRange > 65535:
+            portRange = 65535
+
         try:
-            for port in range(int(portRange)):
+            print(f"\033[1;32m-> Escaneando {sock}... \033[0m")
+            for port in range(portRange+1):
                 try:
                     if port > 0:
-                        s = socket(AF_INET, SOCK_STREAM)
+                        if sock == "UDP":
+                            s = socket(AF_INET, SOCK_DGRAM)
+                        else:
+                            s = socket(AF_INET, SOCK_STREAM)
+
                         s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
                         s.connect( (host, port) )
                         s.close()
@@ -29,21 +37,35 @@ class main:
                     close.append(port)
 
             for i in dispo:
-                print(f"Puerto disponible -> {i}")
+                print(f"\033[1;31m-> Puerto disponible -> {i}")
 
-            print(f"Total de puertos disponibles -> {len(dispo)}")
+            print(f"\033[1;31m-> Total de puertos disponibles ({sock}) -> {len(dispo)} \033[0m")
         
         except ValueError:
-            print("Debe introducir un puerto, no un str.")
+            print("\033[1;31m->Debe introducir un puerto, no un str. \033[0m")
+
 red = main()
 
 if argv[1] in ["--open", "-o"]:
     try:
-        red.openPorts(argv[2], argv[3])
+        if len(argv) == 5:
+            if argv[4] == "-tcp":
+                red.openPorts(argv[2], int(argv[3]), "TCP")
+            elif argv[4] == "-udp":
+                red.openPorts(argv[2], int(argv[3]), "UDP")
+            else:
+                red.openPorts(argv[2], int(argv[3]), "UDP")
+                red.openPorts(argv[2], int(argv[3]), "TCP")
+                exit()
+        else:
+            print("-> No se ha especificado un protocolo, estableciendo a TCP.")
+            red.openPorts(argv[2], int(argv[3]), "TCP")
+
     except IndexError:
-        print(f"Faltó información, abortando operaciones...\nDatos de entrada ->")
+        print(f"\033[1;31m-> Faltó información, abortando operaciones...\nDatos de entrada ->")
         for i in argv[2:]:
             print(i)
+        "\033[0m"
 
 else:
     print("Failed")
