e5c6ac58e0befdc2d3c5c7fd7b745bf987b11857
EssEnemiGz <essenemiggz@gmail.com>
2022-04-17 22:28:03 -0400

    Protocolo UDP eliminado, argumentos -tcp y -udp eliminados, escaneo de puertos individual agregado

--- ports.py
+++ ports.py
@@ -10,7 +10,7 @@ class main:
     def __init__(self):
         print("\033[1;32m-> Escaner de red.\n Por: EssEnemiGz \033[0m")
     
-    def openPorts(self, host, portRange, sock):
+    def openPorts(self, host, portRange):
         dispo = []
         close = []
 
@@ -18,28 +18,22 @@ class main:
             portRange = 65535
 
         try:
-            print(f"\033[1;32m-> Escaneando {sock}... \033[0m")
-            for port in range(portRange+1):
+            print(f"\033[1;32m-> Escaneando puertos con TCP... \033[0m")
+            verification = 0
+            for port in range(portRange):
                 try:
-                    if port > 0:
-                        if sock == "UDP":
-                            s = socket(AF_INET, SOCK_DGRAM)
-                        else:
-                            s = socket(AF_INET, SOCK_STREAM)
+                    s = socket(AF_INET, SOCK_STREAM)
+                    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+                    s.connect( (host, port) )
+                    s.close()
+                    dispo.append(port)
 
-                        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-                        s.connect( (host, port) )
-                        s.close()
-                        dispo.append(port)
-                    else:
-                        pass
+                    print(f"\033[1;31m Puerto disponible -> {dispo[verification]} \033[0m")
+                    verification += 1
                 except OSError:
                     close.append(port)
 
-            for i in dispo:
-                print(f"\033[1;31m-> Puerto disponible -> {i}")
-
-            print(f"\033[1;31m-> Total de puertos disponibles ({sock}) -> {len(dispo)} \033[0m")
+            print(f"\033[1;31m-> Total de puertos disponibles -> {len(dispo)} \033[0m")
         
         except ValueError:
             print("\033[1;31m->Debe introducir un puerto, no un str. \033[0m")
@@ -48,24 +42,18 @@ red = main()
 
 if argv[1] in ["--open", "-o"]:
     try:
-        if len(argv) == 5:
-            if argv[4] == "-tcp":
-                red.openPorts(argv[2], int(argv[3]), "TCP")
-            elif argv[4] == "-udp":
-                red.openPorts(argv[2], int(argv[3]), "UDP")
-            else:
-                red.openPorts(argv[2], int(argv[3]), "UDP")
-                red.openPorts(argv[2], int(argv[3]), "TCP")
-                exit()
-        else:
-            print("-> No se ha especificado un protocolo, estableciendo a TCP.")
-            red.openPorts(argv[2], int(argv[3]), "TCP")
-
+        red.openPorts(argv[2], int(argv[3]))
     except IndexError:
         print(f"\033[1;31m-> Faltó información, abortando operaciones...\nDatos de entrada ->")
         for i in argv[2:]:
-            print(i)
+            print(f"-> {i}")
         "\033[0m"
-
+elif argv[1] in ["--only"]:
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect( (argv[2], int(argv[3])) )
+        print(f"\033[1;31m-> El puerto {argv[3]} está disponible en <<{argv[2]}>> \033[0m")
+    except OSError:
+        print(f"\033[1;31m-> El puerto {argv[3]} no está disponible en <<{argv[2]}>> \033[0m")
 else:
     print("Failed")
