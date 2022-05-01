23f9174485dad148f860d4d11151357573261524
EssEnemiGz <essenemiggz@gmail.com>
2022-05-01 14:18:08 -0400

    Velocidad de escaneo acortada

--- ports.py
+++ ports.py
@@ -5,39 +5,28 @@ By: EssEnemiGz
 
 from socket import *
 from sys import argv
+import threading
+import concurrent.futures
 
 class main:
     def __init__(self):
         print("\033[1;32m-> Escaner de red.\n Por: EssEnemiGz \033[0m")
     
-    def openPorts(self, host, portRange):
-        dispo = []
-        close = []
+    def openPorts(self, host, port):
+        try: 
+            try:
+                s = socket(AF_INET, SOCK_STREAM)
+                s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+                s.settimeout(1)
+                s.connect( (host, port) )
+                s.close()
 
-        if portRange > 65535:
-            portRange = 65535
-        
-        try:
-            print(f"\033[1;32m-> Escaneando puertos con TCP... \033[0m")
-            verification = 0
-            
-            for port in range(portRange):
-                try:
-                    s = socket(AF_INET, SOCK_STREAM)
-                    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-                    s.settimeout(0.3)
-                    s.connect( (host, port) )
-                    s.close()
-                    dispo.append(port)
-
-                    print(f"\033[1;31m Puerto disponible -> {dispo[verification]} \033[0m")
-                    verification += 1
+                print(f"\033[1;31m Puerto disponible -> {port} \033[0m")
+                total.append(port)
                 
-                except OSError:
-                    close.append(port)
+            except OSError:
+                pass
 
-            print(f"\033[1;31m-> Total de puertos disponibles -> {len(dispo)} \033[0m")
-        
         except ValueError:
             print("\033[1;31m->Debe introducir un puerto, no un str. \033[0m")
 
@@ -45,18 +34,30 @@ red = main()
 
 if argv[1] in ["--open", "-o"]:
     try:
-        red.openPorts(argv[2], int(argv[3]))
+        print(f"\033[1;32m-> Escaneando puertos con TCP... \033[0m")
+        print_lock = threading.Lock()
+        total = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            for _port in range(int(argv[3])):
+                executor.submit(red.openPorts, argv[2], _port)
+
+        print(f"\033[1;33m El total de puertos disponibles es -> {len(total)} \033[0m")
+
     except IndexError:
         print(f"\033[1;31m-> Faltó información, abortando operaciones...\nDatos de entrada ->")
         for i in argv[2:]:
             print(f"-> {i}")
         "\033[0m"
+
 elif argv[1] in ["--only"]:
     try:
         s = socket(AF_INET, SOCK_STREAM)
         s.connect( (argv[2], int(argv[3])) )
         print(f"\033[1;31m-> El puerto {argv[3]} está disponible en <<{argv[2]}>> \033[0m")
+    
     except OSError:
         print(f"\033[1;31m-> El puerto {argv[3]} no está disponible en <<{argv[2]}>> \033[0m")
+
 else:
     print("Failed")
